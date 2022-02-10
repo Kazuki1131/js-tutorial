39dcdd5b61c3539caaff4e9f118be5a3eda591b4
Kazuki1131 <onetsubyo@gmail.com>
2022-02-10 21:58:14 +0900

    関数スコープとブロックスコープ

--- 030_スコープ/020_関数スコープとブロックスコープ/end/main.js
+++ 030_スコープ/020_関数スコープとブロックスコープ/end/main.js
@@ -1,17 +1,32 @@
+//関数スコープ
 function a() {
-    let b = 0;
-    console.log(b);
+	let b = 0;
+	console.log(b);
 }
 
-a();
+console.log(b); //関数内で宣言された変数は関数の外から参照できない
 
-if(true) {
-    let c = 1;
-    
-    let d = function() {
-        console.log('d is called');
-    }
+a(); //0
+
+//ブロックスコープ
+if (true) {
+	let c = 1;
+
+	const d = function () {
+		console.log("d is called");
+	};
+
+	var e = 1;
+
+	function f() {
+		console.log("f is called");
+	}
 }
 
-d();
-console.log(c);
+//letやconstはブロックスコープの外ではエラーになる
+console.log(c); // error
+d(); // error
+
+//varや名前つき関数はブロックスコープの外から呼び出せてしまう
+console.log(e); // 1
+f(); // f is called
