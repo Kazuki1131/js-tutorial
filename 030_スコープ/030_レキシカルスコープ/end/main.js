fef36e1cd4c38ccdedb41ea9817b682e70bb71ba
Kazuki1131 <onetsubyo@gmail.com>
2022-02-10 22:17:27 +0900

    レキシカルスコープ

--- 030_スコープ/030_レキシカルスコープ/end/main.js
+++ 030_スコープ/030_レキシカルスコープ/end/main.js
@@ -1,13 +1,11 @@
 let a = 2;
 function fn1() {
-    let b = 1;
-    function fn2() {
-        let c = 3;
-        console.log(b);
-    }
-    fn2();
+	let b = 1;
+	function fn2() {
+		let c = 3;
+		console.log(a); // グローバルスコープの中で変数aを呼び出しているため、参照できる
+		console.log(b); // 関数スコープの中で変数bを呼び出しているため、参照できる
+	}
+	fn2();
 }
 fn1();
-
-
-
