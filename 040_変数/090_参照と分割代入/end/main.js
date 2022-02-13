8b15af4e8a25d8a54cbbc71041c06e9b8882cf58
Kazuki1131 <onetsubyo@gmail.com>
2022-02-13 15:34:03 +0900

    参照と分割代入

--- 040_変数/090_参照と分割代入/end/main.js
+++ 040_変数/090_参照と分割代入/end/main.js
@@ -2,15 +2,16 @@ const a = {
     prop: 0
 }
 
+//オブジェクトのpropプロパティの値をpropという変数に代入
 let { prop } = a;
 
 prop = 1;
 
-console.log(a, prop);
+console.log(a, prop); // a->{prop: 0} prop->1
 
 function fn({ prop }) {
     prop = 1;
-    console.log(a, prop);
+    console.log(a, prop); // a->{prop: 0} prop->1
 }
 
 fn(a);
@@ -25,4 +26,5 @@ let { prop1 } = c;
 
 prop1.prop2 = 1;
 
-console.log(c, prop1);
+// prop1がオブジェクトのため、cのprop2の値も変更される
+console.log(c, prop1); // c->{prop1: {prop2: 1}} prop1->{prop2: 1}
