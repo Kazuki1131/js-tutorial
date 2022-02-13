d77389e89b47e12474b781a4081c98e0188139e8
Kazuki1131 <onetsubyo@gmail.com>
2022-02-13 13:14:24 +0900

    クロージャー

--- 030_スコープ/050_クロージャー（プライベート変数）/end/main.js
+++ 030_スコープ/050_クロージャー（プライベート変数）/end/main.js
@@ -1,8 +1,10 @@
+// クロージャーとは、レキシカルスコープの変数を関数が使用している状態のこと
 function incrementFactory() {
-    
+    //プライベート変数。外部からはアクセスできないようにした関数の中の変数
     let num = 0;
 
     function a() {
+        //レキシカルスコープの変数を関数が使用している
         num = num + 1;
         console.log(num);
     }
@@ -10,6 +12,7 @@ function incrementFactory() {
     return a;
 }
 
+//incrementFactory()が呼び出されたタイミングでのみ、num変数が初期化される
 const increment = incrementFactory();
 
 increment();
--- 030_スコープ/060_クロージャー（動的な関数生成）/end/main.js
+++ 030_スコープ/060_クロージャー（動的な関数生成）/end/main.js
@@ -2,10 +2,15 @@ function addNumberFactory(num) {
     function addNumber(value) {
         return num + value;
     }
+    //addNumberFactory関数が呼び出されるとaddNumber関数を返す
     return addNumber;
 }
 
 const add5 = addNumberFactory(5);
 const add10 = addNumberFactory(10);
-const result = add10(10);
-console.log(result);
+
+const result1 = add5(10);
+const result2 = add10(10);
+
+console.log(result1); //15
+console.log(result2); //20
