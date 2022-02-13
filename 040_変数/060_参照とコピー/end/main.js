33f05cf13a630464d7495989426bec2b4fe4392b
Kazuki1131 <onetsubyo@gmail.com>
2022-02-13 15:00:46 +0900

    参照とコピー

--- 040_変数/060_参照とコピー/end/main.js
+++ 040_変数/060_参照とコピー/end/main.js
@@ -1,12 +1,18 @@
 let a = 'hello';
 let b = a;
 b = 'bye';
-console.log(a, b);
+console.log(a, b); // a->hello b->bye
 
 let c = {
     prop: 'hello'
 }
 let d = c;
-// d.prop = 'bye';
+d.prop = 'bye';
+
+//cの値も変わる！！
+console.log(c, d); // c->{prop: 'bye'} d->{prop: 'bye'}
+
 d = {};
-console.log(c, d);
+
+// dに新しいオブジェクトを設定すれば、cに影響しない
+console.log(c, d); // c->{prop: 'bye'} d->{}
