5afcca037022b278de474f650bca97306b919513
Kazuki1131 <onetsubyo@gmail.com>
2022-02-13 14:06:40 +0900

    即時関数

--- 030_スコープ/070_即時関数/end/main.js
+++ 030_スコープ/070_即時関数/end/main.js
@@ -1,12 +1,14 @@
-function a() {
-	console.log('called');
+// 即時関数(IIDE)とは、関数定義と同時に一度だけ実行される関数。
+function a(a) {
+	console.log('called' + a);
 }
 
-a();
+let b = a(1);
 
-let b = function() {
-	console.log('called');
-}();
+//上記を即時関数で書き直した例
+let b = (function(a) {
+	console.log('called' + a);
+})(1);
 
 let c = (function() {
 
@@ -21,16 +23,18 @@ let c = (function() {
 	function publicFn() {
 		console.log('publicFn is called: ' + privateVal++);
 	}
-	
+
 	return {
 		publicVal,
 		publicFn
 	};
 })()
 
-c.publicFn();
-c.publicFn();
-c.publicFn();
-c.publicFn();
+// privateValの値は変数cに代入された際の一度しか初期化されないため、以下のような出力になる。
+c.publicFn(); // 0
+c.publicFn(); // 1
+c.publicFn(); // 2
+c.publicFn(); // 3
 
-console.log(c.publicVal);
+console.log(c.publicVal); // 10
+console.log(c.privateVal); // error
