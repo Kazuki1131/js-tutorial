16db399eb34cb6134f5c67bf2163af54a650b527
Kazuki1131 <onetsubyo@gmail.com>
2022-02-13 16:08:43 +0900

    関数とオブジェクト

--- 050_関数とオブジェクト１/020_関数とオブジェクト/end/main.js
+++ 050_関数とオブジェクト１/020_関数とオブジェクト/end/main.js
@@ -1,8 +1,15 @@
+// 関数とは"実行可能"な"オブジェクト"である
 function a() {
-    console.log('hello');
+	console.log("hello");
 }
 
+// 関数はオブジェクトとして扱えるため、以下のようにプロパティを指定することができる
 a.prop = 0;
-a.method = function() {
-    console.log('method');
-}
+a.method = function () {
+	console.log("method");
+};
+
+// ()をつけると実行することができる。これが"実行可能"の意味
+a(); // hello
+a.method(); // method
+console.log(a.prop); // 0
