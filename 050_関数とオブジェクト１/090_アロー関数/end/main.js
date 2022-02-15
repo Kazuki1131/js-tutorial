08bdc58bcd77604ebdbaeca3f1218ef28898ea9c
Kazuki1131 <onetsubyo@gmail.com>
2022-02-16 00:41:44 +0900

    アロー関数とthisの挙動

--- 050_関数とオブジェクト１/090_アロー関数/end/main.js
+++ 050_関数とオブジェクト１/090_アロー関数/end/main.js
@@ -2,6 +2,19 @@ function a(name) {
     return 'hello ' + name;
 }
 
+// アロー関数は引数が1つだけの場合は()を省略できる。
+// 処理が1行だけの場合は{}とreturnも省略できる。
 const b = name => 'hello ' + name;
 
+// 引数が2つ以上の場合は()は省略できない
+const c = (name1, name2) => 'hello ' + name1 + ' ' + name2;
+
+// 引数がない場合も()は省略できない
+const d = () => 'hello'
+
 console.log(b('Tom'))
+console.log(c('Tom', 'Bob'))
+console.log(d())
+
+// アロー関数と無名関数の挙動の違い
+// 無名関数はthis, arguments, new, prototypeが使えるが、アロー関数はこれらが使えない
--- 050_関数とオブジェクト１/100_アロー関数とthis/end/main.js
+++ 050_関数とオブジェクト１/100_アロー関数とthis/end/main.js
@@ -1,20 +1,27 @@
 window.name = 'John';
-
-// const a = () => console.log('Bye ' + this.name);
+// このアロー関数のレキシカルスコープはグローバルスコープで、thisはwindowオブジェクトになる
+const a = () => console.log('Bye ' + this.name);
 
 const person = {
     name: 'Tom',
+    // ES6からオブジェクトの無名関数はhello:function(){}を省略してhello(){}のように記述できる
     hello() {
         console.log('Hello ' + this.name);
-        // const a = () => console.log('Bye ' + this.name);
-        // a();
+
+        // このアロー関数のレキシカルスコープはhelloメソッドのスコープで、thisはpersonオブジェクトになる
+        const a = () => console.log('Bye ' + this.name);
+
+        a();
     }
 }
-// person.hello();
+// アロー関数はオブジェクトのメソッドとして呼び出されても、
+// thisが使えないため、グローバルオブジェクトを参照しにいく
+person.hello(); // Hello Tom
 
 function b() {
+    // このアロー関数のレキシカルスコープはb関数のスコープで、thisはwindowオブジェクトになる
     const a = () => console.log('Bye ' + this.name);
     a();
 }
 
-b();
+b(); // Bye John
