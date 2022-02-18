dba99bd71d4906fb3676955b7e7a173b684bad62
Kazuki1131 <onetsubyo@gmail.com>
2022-02-18 23:42:48 +0900

    コンストラクタ関数

--- 060_関数とオブジェクト２/010_コンストラクター関数/end/main.js
+++ 060_関数とオブジェクト２/010_コンストラクター関数/end/main.js
@@ -1,8 +1,11 @@
+// コンストラクタ関数は関数名をパスカルケースで記述するのが一般的
 function Person(name, age) {
+    // this.プロパティ名でインスタンス変数に代入できる
     this.name = name;
     this.age = age;
 }
 
+// コンストラクタ関数はnew演算子でインスタンス化できる
 const bob = new Person('Bob', 18);
 const tom = new Person('Tom', 33);
 const sun = new Person('Sun', 20);
