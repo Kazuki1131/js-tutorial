f34cf81cdc50b9e30a85f06a900943f1b4705b7f
Kazuki1131 <onetsubyo@gmail.com>
2022-02-18 23:44:25 +0900

    instanceof

--- 060_関数とオブジェクト２/040_instanceof/end/main.js
+++ 060_関数とオブジェクト２/040_instanceof/end/main.js
@@ -1,9 +1,12 @@
 function F(a, b) {
-    this.a = a;
-    this.b = b;
-    // const result = new Object();
-    // result.a = 1;
-    // return result;
+	this.a = a;
+	this.b = b;
+	// return {a:1}は下記と同じ意味になる。
+	// const result = new Object();
+	// result.a = 1;
+	// return result;
+	// このようにあたしいオブジェクトを生成するため、インスタンス instanceof F としてもfalseになる
+	return { a: 1 };
 }
 
 F.prototype.c = function() {}
@@ -16,8 +19,10 @@ console.log(instance.__proto__ === F.prototype)
 
 function fn(arg) {
     if(arg instanceof Array) {
+        // argが配列だった場合はpush関数が使える
         arg.push('value');
     } else {
+        // argがオブジェクトだった場合はpushは使えないため、以下のように値を追加する
         arg['key'] = 'value';
     }
     console.log(arg)
