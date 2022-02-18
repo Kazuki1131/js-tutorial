607d352dde0a055cd3cafb34f258f16c86711b0d
Kazuki1131 <onetsubyo@gmail.com>
2022-02-18 23:43:15 +0900

    prototype

--- 060_関数とオブジェクト２/020_prototype/end/main.js
+++ 060_関数とオブジェクト２/020_prototype/end/main.js
@@ -1,11 +1,16 @@
 function Person(name, age) {
     this.name = name;
     this.age = age;
-    // this.hello = function() {
-    //     console.log('hello ' + this.name);
-    // }
+    // コンストラクタ関数で関数を定義するときは以下のようにするのではなく、後述のprototypeを使う
+    // 理由：メモリの消費が抑えられるため。
+    // メモリ消費が抑えられる理由は、以下のように定義するとインスタンス生成ごとに関数オブジェクトが生成されるが
+    // prototypeだと関数を参照しているprototypeオブジェクトがインスタンス生成時に引き継がれるだけで、関数を生成しないため
+    this.hello = function() {
+        console.log('hello ' + this.name);
+    }
 }
 
+// コンストラクタ関数名.prototype.関数名 = 無名関数 でコンストラクタ関数に関数を定義できる
 Person.prototype.hello = function() {
     console.log('hello ' + this.name);
 }
@@ -14,5 +19,8 @@ const bob = new Person('Bob', 18);
 const tom = new Person('Tom', 33);
 const sun = new Person('Sun', 20);
 
+// hello Bob
 bob.hello();
+
+// hello Tom
 tom.hello();
