03d21876cf10667b8435e6411f25753eca9161ac
Kazuki1131 <onetsubyo@gmail.com>
2022-02-19 18:49:21 +0900

    ループ文

--- 070_反復処理/025_ループ文とブロックスコープ/end/main.js
+++ 070_反復処理/025_ループ文とブロックスコープ/end/main.js
@@ -1,4 +1,5 @@
 for(let i = 0; i < 5; i++) {
+	// for文ではオブジェクトが再生成されるため、constが記述できる
 	const j = i * 2;
 	setTimeout(function() {
 		console.log(j);
--- 070_反復処理/030_配列とループ文/end/main.js
+++ 070_反復処理/030_配列とループ文/end/main.js
@@ -4,6 +4,7 @@ for(let i = 0; i < arry.length; i++) {
 	console.log(arry[i]);
 }
 
+// 以下は let v; let i = 0; と同義
 let v, i = 0;
 while(v = arry[i++]) {
 	console.log(v);
