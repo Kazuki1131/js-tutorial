fb0fc5ffdecab5cfc1c478e00f8afbccb2627bac
Kazuki1131 <onetsubyo@gmail.com>
2022-02-19 22:54:12 +0900

    タスクキューとコールスタック

--- 080_非同期処理/020_タスクキューとコールスタック/end/main.js
+++ 080_非同期処理/020_タスクキューとコールスタック/end/main.js
@@ -1,10 +1,14 @@
+/**
+ * タスクキューとは、実行待ちの非同期処理の行列のこと。つまり、非同期処理の実行順を管理している。
+ * キューは先入先出(FIFO)である
+ */
 const btn = document.querySelector('button');
 btn.addEventListener('click', function task2() {
   console.log('task2 done');
 });
 
 function a() {
-  setTimeout(function task1() { 
+  setTimeout(function task1() {
     console.log('task1 done');
   }, 4000);
 
