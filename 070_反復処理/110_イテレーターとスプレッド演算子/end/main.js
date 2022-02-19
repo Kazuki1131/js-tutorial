5aa6d60c02dc339890c3e49eeb1a16981b2182c8
Kazuki1131 <onetsubyo@gmail.com>
2022-02-19 22:15:32 +0900

    イテレーターとスプレッド構文

--- 070_反復処理/110_イテレーターとスプレッド演算子/end/main.js
+++ 070_反復処理/110_イテレーターとスプレッド演算子/end/main.js
@@ -1,8 +1,12 @@
+// スプレッド演算子とは、反復可能や列挙可能オブジェクトの展開を行う演算子
+// ただし、関数の仮引数で使用された場合は、実引数に渡された変数を配列にまとめる挙動に変わる(レストパラメーターという)
 const arry1 = [1,2,3,4,5];
 const arry2 = [0, ...arry1, 6];
 arry2.push(6)
-console.log(arry2);
-console.log(arry1 === arry2);
+// arry2にpushで値を追加してもarry1には影響しない
+console.log(arry1); //[1,2,3,4,5]
+console.log(arry2); //[1,2,3,4,5,6]
+console.log(arry1 === arry2); // false
 
 function sum(...args) {
   let ret = 0;
@@ -20,16 +24,22 @@ const obj1 = {
 	prop3: 'value3'
 }
 
+// TypeError: object is not iterable (反復可能オブジェクトではないというエラーが出る)
+const arry3 = [ ...obj1 ];
+
+// オブジェクトをイテレートするには、オブジェクトのprototypeのsymbol iteratorにイテレーター（ジェネレーター）を追加する
 Object.prototype[Symbol.iterator] = function*() {
 	for(let key in this) {
 		yield [key, this[key]];
 	}
 }
 
+// エラーは出ない
 const arry3 = [ ...obj1 ];
 
 console.log(arry3);
 
+// オブジェクトリテラルでスプレッド演算子を使う場合は、symbol iteratorに追加する必要はない(追加するのは配列の時だけ)
 const obj2 = {...obj1};
 
 console.log(obj2);
--- 070_反復処理/920_修了確認_ジェネレーター/end/main.js
+++ 070_反復処理/920_修了確認_ジェネレーター/end/main.js
@@ -2,22 +2,25 @@
  * 問題：
  * 引数で与えた範囲の値をステップ毎に返却する
  * genStepというジェネレーター関数を作成しましょう。
- * 
+ *
  * - genStepの要件
  * 引数にmin, max, stepを取ります。
  * min：下限値
  * max：上限値
  * step：ステップ
- * 
+ *
  * 以下のように実行した場合には
  * const it = genStep({min: 4, max: 10, step: 2});
- * 
+ *
  * for(let value of it) {
  *   console.log(value); -> 4, 6, 8, 10
  * }
- * 
+ *
  * の値が順番にコンソールに表示されます。
  */
+// 引数に以下のように書くことで、各引数のデフォルト引数を設定でき、
+// 引数が全く渡ってこなかったときは空のオブジェクトをデフォルト引数に設定できる
+// デフォルト引数にInfinityをつけることで上限なしに設定できる
 function* genStep({min = 0, max = Infinity, step = 1} = {}) {
   for(let i = min; i <= max; i += step) {
     yield i;
