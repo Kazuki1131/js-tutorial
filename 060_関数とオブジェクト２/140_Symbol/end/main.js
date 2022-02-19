1ae28d1d950765d450450a7135fe85d8b16469c3
Kazuki1131 <onetsubyo@gmail.com>
2022-02-19 10:43:11 +0900

    Symbol

--- 060_関数とオブジェクト２/140_Symbol/end/main.js
+++ 060_関数とオブジェクト２/140_Symbol/end/main.js
@@ -1,5 +1,12 @@
+// ES6から導入されたシンボルとは、プロパティの重複を避けるために、必ず一意の値を返す関数
 const s = Symbol('hello');
-console.log(s);
+const s2 = Symbol('hello');
+
+// 同じ文字列でも異なる値として扱われる
+console.log(s === s2); // false
+
+// symbolはプリミティブ型の一種
+console.log(typeof s); // symbol
 
 const str = new String('Tom');
 console.log(str);
@@ -8,5 +15,5 @@ String.prototype[s] = function() {
        return 'hello ' + this;
 }
 
-const tom = 'Tom'; 
+const tom = 'Tom';
 console.log(tom[s]());
