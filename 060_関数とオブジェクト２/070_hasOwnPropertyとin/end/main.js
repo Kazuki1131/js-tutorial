33c59c7493573276e329bb56daf066eb22a9d332
Kazuki1131 <onetsubyo@gmail.com>
2022-02-19 09:01:44 +0900

    hasOwnPropertyとin

--- 060_関数とオブジェクト２/070_hasOwnPropertyとin/end/main.js
+++ 060_関数とオブジェクト２/070_hasOwnPropertyとin/end/main.js
@@ -8,6 +8,10 @@ Object.prototype.hello = function() {
 }
 
 const bob = new Person('Bob', 18);
+
+//自分自身のオブジェクトに引数で与えられたプロパティかメソッドが存在するか確認できる
 const result = bob.hasOwnProperty('hello')
-console.log(result)
-console.log('hasOwnProperty' in bob);
+console.log(result) // true
+
+// in はプロトタイプチェーンまで含めて指定された名前のメソッドやプロパティが存在するかを確認できる
+console.log('hasOwnProperty' in bob); // ture
