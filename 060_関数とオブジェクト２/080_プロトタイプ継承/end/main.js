77c7e0214340ecb310255eaaece658174c13f4a1
Kazuki1131 <onetsubyo@gmail.com>
2022-02-19 09:12:10 +0900

    プロトタイプ継承

--- 060_関数とオブジェクト２/080_プロトタイプ継承/end/main.js
+++ 060_関数とオブジェクト２/080_プロトタイプ継承/end/main.js
@@ -7,17 +7,21 @@ Person.prototype.hello = function() {
     console.log('hello ' + this.name);
 }
 
+// Personにはない新しい引数(gender)を定義することができる
 function Japanese(name, age, gender) {
+    // thisによってJapaneseの関数コンテキストがPersonに渡る
     Person.call(this, name, age);
     this.gender = gender;
 }
 
+// JapaneseのprototypeにPersonのオブジェクト＋prototypeを継承する
 Japanese.prototype = Object.create(Person.prototype);
 
 Japanese.prototype.hello = function() {
     console.log('Konnichiwa ' + this.name);
 }
 
+// Personにはない新しい関数を定義することもできる
 Japanese.prototype.bye = function() {
     console.log('Sayonara ' + this.name);
 }
@@ -25,5 +29,8 @@ Japanese.prototype.bye = function() {
 const taro = new Japanese('Taro', 23, 'Male');
 console.log(taro);
 
+// Konnichiwa Taro
 taro.hello();
+
+// Sayonara Taro
 taro.bye();
