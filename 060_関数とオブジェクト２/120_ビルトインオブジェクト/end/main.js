38ab0791e543f067e7280dd88c8f5cae6f8095cc
Kazuki1131 <onetsubyo@gmail.com>
2022-02-19 09:58:02 +0900

    ビルトインオブジェクトとラッパーオブジェクト

--- 060_関数とオブジェクト２/120_ビルトインオブジェクト/end/main.js
+++ 060_関数とオブジェクト２/120_ビルトインオブジェクト/end/main.js
@@ -1,3 +1,4 @@
+// ビルトインオブジェクトとは、コード実行前にJSエンジンによって自動的に生成されるオブジェクトのこと
 const arry = new Array(1,2,3,4);
 console.log(arry);
 console.log(arry["0"]);
--- 060_関数とオブジェクト２/130_ラッパーオブジェクト/end/main.js
+++ 060_関数とオブジェクト２/130_ラッパーオブジェクト/end/main.js
@@ -1,5 +1,11 @@
+// ラッパーオブジェクトとはプリミティブ値を便利に操作できるようにしたプリミティブ値を内包したオブジェクト
+// 下記は a = 'hello' と同等
 const a = new String('hello');
-console.log(a.toUpperCase());
 
+// toUpperCaseは全て大文字にする
+console.log(a.toUpperCase()); // HELLO
+
+// 下記は b = 100 と同等
 const b = new Number(100);
-console.log(b);
+// 100を10の2乗として変換できる
+console.log(b.toExponential); // 1e+2
