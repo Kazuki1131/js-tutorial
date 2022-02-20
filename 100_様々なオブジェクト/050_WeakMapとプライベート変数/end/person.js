1ea473e8a6859707da37c978badfa36db4fead6f
Kazuki1131 <onetsubyo@gmail.com>
2022-02-20 17:47:03 +0900

    WeakMap

--- 100_様々なオブジェクト/050_WeakMapとプライベート変数/end/person.js
+++ 100_様々なオブジェクト/050_WeakMapとプライベート変数/end/person.js
@@ -2,13 +2,15 @@ const wm = new WeakMap();
 
 export class Person {
     constructor(name) {
-        // this._name = name;
+        // this._name = name;だとプライベート変数にしたいのに外部からアクセスできてしまう
+        //下記のようにWeakMapを使用することで、外部からアクセスできないようにできる
         wm.set(this, {
             name
         });
     }
 
     hello() {
+        // getメソッドで呼び出す
         console.log(`hello ${wm.get(this).name}`);
     }
 }
