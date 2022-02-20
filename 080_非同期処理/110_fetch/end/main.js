0d45e7615a67b04eb9c4306fad9e675f243f2bf0
Kazuki1131 <onetsubyo@gmail.com>
2022-02-20 09:32:32 +0900

    fetch

--- 080_非同期処理/110_fetch/end/main.js
+++ 080_非同期処理/110_fetch/end/main.js
@@ -1,3 +1,14 @@
+/**
+ * fetchを使うことで、サーバー上からデータを取得することができる
+ * 今回は、users.jsonのデータを取得する
+ * jsonは、jsからサーバー上にデータをとりに行くときや、
+ * サーバーに対してデータをリクエストするときによく使われる形式
+ * 基本的にjsのオブジェクトや配列と記述が似ているが、以下の3つのルールがある
+ * 1. シングルクォーテーションで文字列を囲むことはできない。必ずダブルクォーテーションで囲む。
+ * 2. プロパティは必ずダブルクォーテーションで囲む
+ * 3. 格納されている一番最後の要素には , (カンマ)をつけてはいけない
+ */
+
 // fetch('users.json').then(function(response) {
 //   console.log(response.json());
 //   return response.json();
@@ -8,6 +19,8 @@
 //   }
 // })
 
+// fetchは以上のコメントアウトしたように記述できるが、
+// fetchはPromiseインスタンスのため、async, awaitで以下のように書き換えられる。
 async function fetchUsers() {
   const response = await fetch('users.json');
   const json = await response.json();
