c517bcfeae89829ccb69991c75b3ebe57d93d250
Kazuki1131 <onetsubyo@gmail.com>
2022-02-20 13:22:16 +0900

    ダイナミックインポート

--- 090_モジュラーJavaScript/070_ダイナミックインポート/end/moduleB.js
+++ 090_モジュラーJavaScript/070_ダイナミックインポート/end/moduleB.js
@@ -1,3 +1,11 @@
+/**
+ * ダイナミックインポートとは、importを非同期で読み込むことができる
+ * そのため、画面の初期表示時には必要ないものを、必要なときにimportするようにできるため
+ * 画面の初期表示が速くなる。
+ * 新しい機能のため、webpackやbabelなどを使うか、対応ブラウザのみで使うようにする
+ */
+//
+//
 async function fn() {
   const modules = await import('./moduleA.js');
   modules.publicFn();
