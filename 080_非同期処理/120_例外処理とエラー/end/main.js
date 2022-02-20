51f23e0c2700b07207d393783c75f57735ad5858
Kazuki1131 <onetsubyo@gmail.com>
2022-02-20 09:57:20 +0900

    例外処理とエラー

--- 080_非同期処理/120_例外処理とエラー/end/main.js
+++ 080_非同期処理/120_例外処理とエラー/end/main.js
@@ -1,14 +1,29 @@
+/**
+ * 例外処理とは、エラーが発生した際に飛ぶ特別な処理
+ * 以下のように記述する
+ * try {
+ *  throw new Error()
+ * } catch {
+ *  エラーハンドリング
+ * } finally {
+ *  終了処理
+ * }
+ */
+
 async function fetchUsers() {
   const response = await fetch('users.json');
-  if(response.ok) {
+
+  if(response.ok) { // okプロパティがtrueだった場合に実行される
     const json = await response.json();
-    if(!json.length) {
+
+    if(!json.length) { // jsonに要素がなかった場合にthrowされる
       throw new Error('no data found');
     }
     return json;
   }
 }
 
+// 以下のようにErrorクラスを継承することでカスタムエラーを作成することができる
 class NoDataError extends Error {
   constructor(message) {
     super(message);
@@ -17,20 +32,21 @@ class NoDataError extends Error {
 }
 
 async function init() {
-  // try {
+  try {
     const users = await fetchUsers();
     for(const user of users) {
       console.log(`I'm ${user.name}, ${user.age} years old`)
     }
-  // } catch(e) {
-  //   if(e instanceof NoDataError) {
-  //     console.error(e);
-  //   } else {
-  //     console.error('Oops, something went wrong');
-  //   }
-  // } finally {
-  //   console.log('bye');
-  // }
+  } catch(e) {
+    // instanceofでエラーの種類に応じて条件分岐できる
+    if(e instanceof NoDataError) {
+      console.error(e);
+    } else {
+      console.error('Oops, something went wrong');
+    }
+  } finally {
+    console.log('bye');
+  }
   console.log('end');
 }
 init();
